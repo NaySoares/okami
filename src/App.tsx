d24f31cfb5abb0f062b4bcfd4729952a32283676
Elienai <elienay.soares07@gmail.com>
2021-11-27 09:51:34 -0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 import { Home } from './pages/Home';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+import discordDark from '../../assets/icons/iconDiscordDark.svg';
+import facebookDark from '../../assets/icons/iconFacebookDark.svg';
+import twitterDark from '../../assets/icons/iconTwitterDark.svg';
+
+import './Footer.scss'
+
+export function Footer() {
+  return(
+    <footer>
+      <section className="socialFooter">
+        <a href="#">
+          <img src={discordDark} alt="Icone do Discord" />
+        </a>
+        <a href="#">
+          <img src={facebookDark} alt="Icone do Facebook" />
+        </a>
+        <a href="#">
+          <img src={twitterDark} alt="Icone do Twitter" />
+        </a>
+      </section>
+      <ul className="menuFooter">
+        <li>
+          <a href="#">Light Novel</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Equipe</a>
+        </li>
+        <li>
+          <a href="#">Apoio</a>
+        </li>
+      </ul>
+      <strong>Copyright © 2021 Okami</strong>
+    </footer>
+  )
+}
