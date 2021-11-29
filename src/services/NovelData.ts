3a0606f4de7999673f53baee90d6b32f21cedeb6
Elienai <elienay.soares07@gmail.com>
2021-11-29 20:23:11 -0300

    att data

--- src/services/NovelData.ts
+++ src/services/NovelData.ts
@@ -2,6 +2,7 @@ import cover01 from "../assets/images/coverMatadorGoblinsV7.jpg";
 import cover02 from "../assets/images/coverBruxaErranteV6.jpg";
 import cover03 from "../assets/images/coverMushokuTenseiV14.jpg";
 import cover04 from "../assets/images/coverCronicasBehamutV9.jpg";
+import bannerElaina from '../assets/banner/bannerMajoNoTabitabi.png'
 
 interface NovelDataProps {
   id: number;
@@ -20,6 +21,9 @@ interface NovelDataProps {
   currentVol: string;
   currentCap: string;
   status: string;
+  extra: boolean;
+  contentExtra: string;
+  banner?: string;
 }
 
 export const NovelData: NovelDataProps[] = [
@@ -35,7 +39,7 @@ export const NovelData: NovelDataProps[] = [
     "— Não posso salvar o mundo, só posso matar goblins.",    
     "Se dizia que, em uma guilda perto da fronteira, havia um homem que só caçava goblins e se elevou ao ranque prata (3º ranque). Ele era esse tipo de existência rara…",
     "Durante a primeira aventura em grupo de uma sacerdotisa, quando ela ficou em perigo, esse homem — o “matador de goblins” — estendeu sua mão para ajudá-la. Ele faria qualquer coisa e tudo mais para exterminar goblins.",
-    "A sacerdotisa foi salva por ele, a recepcionista da guilda estava grata por ele, e sua amiga de infância — uma vaqueira — estava à espera do seu regresso. Foi então que, uma elfa jovem que ouve falar dos seus feitos apareceu e fez um pedido."].join("\n"),
+    "A sacerdotisa foi salva por ele, a recepcionista da guilda estava grata por ele, e sua amiga de infância — uma vaqueira — estava à espera do seu regresso. Foi então que, uma elfa jovem que ouve falar dos seus feitos apareceu e fez um pedido."].join("<br/><br/>"),
     date: "8 de Novembro de 2008",
     author: "Kumo Kagyu (蜗牛くも)",
     artist: "Desconhecido",
@@ -45,6 +49,8 @@ export const NovelData: NovelDataProps[] = [
     currentVol: "Volume 05",
     currentCap: "Capítulo 05 - Parte 4",
     status: "Em Andamento",
+    extra: true,
+    contentExtra: "SFMusDom-eo",
   },
   {
     id: 14,
@@ -58,7 +64,7 @@ export const NovelData: NovelDataProps[] = [
       "Um país que só aceitava magos, um gigante obcecado por músculos, um jovem às portas da morte aguardando o retorno de sua amada, uma princesa deixada sozinha no país em ruínas e a história da própria bruxa até agora e daqui para frente.",
       "Enquanto conhece pessoas particularmente peculiares e experimenta os momentos de alegria de outras pessoas, mesmo agora, a bruxa continua contando a história do encontro e da separação.",
       "— Por favor, não se preocupe. Afinal, sou uma viajante. Devo me apressar.",      
-      "A Bruxa, Sim, eu."].join("\n"),
+      "A Bruxa, Sim, eu."].join('<br/><br/>'),
     date: "1 de Maio de 2013",
     author: "Shiraishi Jougi",
     artist: "Azure",
@@ -68,6 +74,9 @@ export const NovelData: NovelDataProps[] = [
     currentVol: "Volume 06",
     currentCap: "Capítulo 03",
     status: "Em Andamento",
+    extra: true,
+    contentExtra: "L9RDSB7LrEI",
+    banner: bannerElaina,
   },
   {
     id: 15,
@@ -78,7 +87,7 @@ export const NovelData: NovelDataProps[] = [
     genre: ["Ação", "Aventura", "Comédia", "Ecchi", "Drama", "Fantasia", "Harém", "Isekai", "Romance", "Seinen"],
     description: ["Um NEET otaku de 34 anos, expulso de casa por sua família, descobriu que sua vida chegou a um beco sem saída. Ele então lembrou que sua vida poderia ter sido realmente muito melhor se tivesse feito escolhas melhores no passado.",
     "Apenas quando chegou ao ponto de se arrepender, viu um caminhão correndo em direção de três estudantes do ensino médio. Reunindo toda a força que tinha, tentou salvá-los e acabou sendo atropelado, assim rapidamente perdendo sua vida.",
-    "E quando voltou a abrir os olhos, tinha reencarnado em um mundo com magias e espadas como Rudeus Greyrat. Nascido em um novo mundo, com uma nova vida, decidiu que: “Desta vez vou realmente viver minha vida ao máximo, sem arrependimentos!” Assim começa a jornada de um homem que anseia pelo recomeço de sua vida."].join("\n"),
+    "E quando voltou a abrir os olhos, tinha reencarnado em um mundo com magias e espadas como Rudeus Greyrat. Nascido em um novo mundo, com uma nova vida, decidiu que: “Desta vez vou realmente viver minha vida ao máximo, sem arrependimentos!” Assim começa a jornada de um homem que anseia pelo recomeço de sua vida."].join("<br/><br/>"),
     date: "18 de Abril de 2002",
     author: "Kumo Kagyu (蜗牛くも)",
     artist: "Shirotaka",
@@ -88,19 +97,21 @@ export const NovelData: NovelDataProps[] = [
     currentVol: "Volume 13",
     currentCap: "Capítulo 13",
     status: "Em Andamento",
+    extra: true,
+    contentExtra: "Qx01pn9l-6g",
   },
   {
     id: 16,
     img: cover04,
     title: "Crônicas do Bahamut Invicto",
-    titleAlternative: ["Saijaku Muhai no Bahamu", "Undefeated Bahamut Chronicle"],
+    titleAlternative: ["Saijaku Muhai no Bahamut", "Undefeated Bahamut Chronicle"],
     type: "Light Novel",
     genre: ["Ecchi", "Fantasia", "Harém", "Mecha", "Vida Escolar"],
     description: ["Lux, que era um Príncipe do Império que foi destruído há cinco anos, encontrou-se com a Princesa do Novo Reino, Lisesharte, invadindo acidentalmente o banheiro do dormitório feminino.",
     '"Quanto tempo você planeja olhar para o meu corpo, seu idiotaaaaa!”',
     "As armas antigas encontradas nas Ruínas, o Drag-Ride. Lux que foi chamado o mais forte Drag-Knight no passado, agora era conhecido como o “Mais Fraco Invicto”, um Drag-Knight que não ataca. Após o duelo que começou com o desafio de Lisesharte, Lux acabou entrando na academia feminina que cria Drag-Knights……!",
     "A história de Lux, onde ele está cercado por meninas nobres da academia, está prestes a se desdobrar.",
-    'A “derradeira” batalha de fantasia da academia, onde o direito e o poder se cruzam, começa agora! (Sinopse da história retirada da capa do Volume 1.)'].join("\n"),
+    'A “derradeira” batalha de fantasia da academia, onde o direito e o poder se cruzam, começa agora! (Sinopse da história retirada da capa do Volume 1.)'].join("<br/><br/>"),
     date: "28 de Dezembro de 2010",
     author: "Akatsuki Senri",
     artist: "Kasuga Ayumu",
@@ -110,71 +121,7 @@ export const NovelData: NovelDataProps[] = [
     currentVol: "Volume 02",
     currentCap: "Capítulo 03 - Parte 4",
     status: "Em Andamento",
+    extra: true,
+    contentExtra: "46hN_ol_g7I",
   },
-  {
-    id: 20,
-    img: cover04,
-    title: "Crônicas do Bahamut Invicto",
-    titleAlternative: ["Saijaku Muhai no Bahamu", "Undefeated Bahamut Chronicle"],
-    type: "Light Novel",
-    genre: ["Ecchi", "Fantasia", "Harém", "Mecha", "Vida Escolar"],
-    description: ["Lux, que era um Príncipe do Império que foi destruído há cinco anos, encontrou-se com a Princesa do Novo Reino, Lisesharte, invadindo acidentalmente o banheiro do dormitório feminino.",
-    '"Quanto tempo você planeja olhar para o meu corpo, seu idiotaaaaa!”',
-    "As armas antigas encontradas nas Ruínas, o Drag-Ride. Lux que foi chamado o mais forte Drag-Knight no passado, agora era conhecido como o “Mais Fraco Invicto”, um Drag-Knight que não ataca. Após o duelo que começou com o desafio de Lisesharte, Lux acabou entrando na academia feminina que cria Drag-Knights……!",
-    "A história de Lux, onde ele está cercado por meninas nobres da academia, está prestes a se desdobrar.",
-    'A “derradeira” batalha de fantasia da academia, onde o direito e o poder se cruzam, começa agora! (Sinopse da história retirada da capa do Volume 1.)'].join("\n"),
-    date: "28 de Dezembro de 2010",
-    author: "Akatsuki Senri",
-    artist: "Kasuga Ayumu",
-    imgAuthor: "https://github.com/NaySoares.png",
-    imgArtist: "https://github.com/NaySoares.png",
-    statusTop: true,
-    currentVol: "Volume 02",
-    currentCap: "Capítulo 03 - Parte 4",
-    status: "Em Andamento",
-  },
-  {
-    id: 21,
-    img: cover04,
-    title: "Crônicas do Bahamut Invicto",
-    titleAlternative: ["Saijaku Muhai no Bahamu", "Undefeated Bahamut Chronicle"],
-    type: "Light Novel",
-    genre: ["Ecchi", "Fantasia", "Harém", "Mecha", "Vida Escolar"],
-    description: ["Lux, que era um Príncipe do Império que foi destruído há cinco anos, encontrou-se com a Princesa do Novo Reino, Lisesharte, invadindo acidentalmente o banheiro do dormitório feminino.",
-    '"Quanto tempo você planeja olhar para o meu corpo, seu idiotaaaaa!”',
-    "As armas antigas encontradas nas Ruínas, o Drag-Ride. Lux que foi chamado o mais forte Drag-Knight no passado, agora era conhecido como o “Mais Fraco Invicto”, um Drag-Knight que não ataca. Após o duelo que começou com o desafio de Lisesharte, Lux acabou entrando na academia feminina que cria Drag-Knights……!",
-    "A história de Lux, onde ele está cercado por meninas nobres da academia, está prestes a se desdobrar.",
-    'A “derradeira” batalha de fantasia da academia, onde o direito e o poder se cruzam, começa agora! (Sinopse da história retirada da capa do Volume 1.)'].join("\n"),
-    date: "28 de Dezembro de 2010",
-    author: "Akatsuki Senri",
-    artist: "Kasuga Ayumu",
-    imgAuthor: "https://github.com/NaySoares.png",
-    imgArtist: "https://github.com/NaySoares.png",
-    statusTop: true,
-    currentVol: "Volume 02",
-    currentCap: "Capítulo 03 - Parte 4",
-    status: "Em Andamento",
-  },
-  {
-    id: 22,
-    img: cover04,
-    title: "Crônicas do Bahamut Invicto",
-    titleAlternative: ["Saijaku Muhai no Bahamu", "Undefeated Bahamut Chronicle"],
-    type: "Light Novel",
-    genre: ["Ecchi", "Fantasia", "Harém", "Mecha", "Vida Escolar"],
-    description: ["Lux, que era um Príncipe do Império que foi destruído há cinco anos, encontrou-se com a Princesa do Novo Reino, Lisesharte, invadindo acidentalmente o banheiro do dormitório feminino.",
-    '"Quanto tempo você planeja olhar para o meu corpo, seu idiotaaaaa!”',
-    "As armas antigas encontradas nas Ruínas, o Drag-Ride. Lux que foi chamado o mais forte Drag-Knight no passado, agora era conhecido como o “Mais Fraco Invicto”, um Drag-Knight que não ataca. Após o duelo que começou com o desafio de Lisesharte, Lux acabou entrando na academia feminina que cria Drag-Knights……!",
-    "A história de Lux, onde ele está cercado por meninas nobres da academia, está prestes a se desdobrar.",
-    'A “derradeira” batalha de fantasia da academia, onde o direito e o poder se cruzam, começa agora! (Sinopse da história retirada da capa do Volume 1.)'].join("\n"),
-    date: "28 de Dezembro de 2010",
-    author: "Akatsuki Senri",
-    artist: "Kasuga Ayumu",
-    imgAuthor: "https://github.com/NaySoares.png",
-    imgArtist: "https://github.com/NaySoares.png",
-    statusTop: true,
-    currentVol: "Volume 02",
-    currentCap: "Capítulo 03 - Parte 4",
-    status: "Em Andamento",
-  }
 ]
