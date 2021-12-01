1020b2472f39b92075d75e1acfcaa060eedb236f
Elienai <elienay.soares07@gmail.com>
2021-12-01 11:21:11 -0300

    add chapter content

--- src/pages/ChapterPage.tsx
+++ src/pages/ChapterPage.tsx
@@ -1,5 +1,24 @@
+import { useParams } from 'react-router';
+import { NovelData } from '../services/NovelData';
+import { Banner } from '../components/Project/Banner/Banner';
+import { ChapterContent } from '../components/Chapter/ChapterContent/ChapterContent';
+import '../styles/chapterPage.scss';
+interface NovelProps {
+  id: number;
+  banner?: string;
+}
+
 export function ChapterPage() {
+  const { id, volume, chapterId }= useParams();
+  const currentWork: NovelProps[] = NovelData.filter((obj) => obj.id === Number(id));
+  let banner = '';
+  currentWork[0].banner ? banner = currentWork[0].banner : banner = '';
   return(
-    <h1>Página do Capitulo</h1>
+    <main className="chapterPageContainer">
+      <Banner currentBanner={banner} />
+      <div className="chapterPageContent">
+        <ChapterContent />
+      </div>
+    </main>
   )
 }
