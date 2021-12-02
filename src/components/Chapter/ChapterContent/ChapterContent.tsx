48242a3bde0271e19421ee7864d3c8c65165e2d5
Elienai <elienay.soares07@gmail.com>
2021-12-02 09:03:15 -0300

    remove event click/add changeFontSize

--- src/components/Chapter/ChapterContent/ChapterContent.tsx
+++ src/components/Chapter/ChapterContent/ChapterContent.tsx
@@ -1,7 +1,6 @@
-import { title } from 'process';
-import { useEffect, useState } from 'react';
-import './ChapterContent.scss';
-interface ChapterContentProps{
+import { useEffect, useState } from "react";
+import "./ChapterContent.scss";
+interface ChapterContentProps {
   theLast: boolean;
   theFirst: boolean;
   goPrev: () => void;
@@ -12,25 +11,55 @@ interface ChapterContentProps{
   titleChapter: string;
 }
 
-export function ChapterContent({theLast, theFirst, content, titleNovel, titleChapter, goPrev, goNext, goMenu} : ChapterContentProps) {
-  const [progressBar, setProgressBar] = useState (0)
+export function ChapterContent({
+  theLast,
+  theFirst,
+  content,
+  titleNovel,
+  titleChapter,
+  goPrev,
+  goNext,
+  goMenu,
+}: ChapterContentProps) {
+  const [progressBar, setProgressBar] = useState(0);
+  const [fontSize, setFontSize] = useState(18);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   const onScroll = () => {
     const winScroll = document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = ((winScroll / height) * 100);
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
     setProgressBar(scrolled);
+  };
+
+  const changeFontSize = ( operator: string ) => {
+    if(operator === "-"){
+      const newFontSize = fontSize - 1;
+        if (newFontSize >= 12){
+          setFontSize(newFontSize);
+        }
+    } else if( operator === "+"){
+      const newFontSize = Number(fontSize) + 1;
+        if(newFontSize <= 26){
+          setFontSize(newFontSize);
+        }
+    }
   }
 
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll)
-  }, [])
+  const nextChapter = "Próximo >";
+  const prevChapter = "< Anterior";
 
-  const nextChapter = "Próximo >"
-  const prevChapter = "< Anterior"
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault()
+  });
 
-  return(
+  return (
     <div className="chapterContentContainer">
       <header className="chapterContentHeader">
         <h1>{titleChapter}</h1>
@@ -38,42 +67,47 @@ export function ChapterContent({theLast, theFirst, content, titleNovel, titleCha
       </header>
       <section className="chapterContentNavigation">
         <div className="chapterContentControllerSize">
-          <strong>+</strong>
-          <strong>-</strong>
+          <strong onClick={() => changeFontSize("+")}>+</strong>
+          <strong onClick={() => changeFontSize("-")}>-</strong>
         </div>
         <div className="chapterContentControllerChapter">
-          {theFirst ? 
+          {theFirst ? (
             <strong onClick={goMenu}>Retornar ao menu</strong>
-          : 
+          ) : (
             <strong onClick={goPrev}>{prevChapter}</strong>
-          }
-          {theLast ? 
+          )}
+          {theLast ? (
             <strong onClick={goMenu}>Retornar ao menu</strong>
-          :
+          ) : (
             <strong onClick={goNext}>{nextChapter}</strong>
-          } 
+          )}
         </div>
       </section>
-      <span dangerouslySetInnerHTML={{__html: content? content : 'Ops... Algo deu errado.' }}></span>
+      <span 
+        style={{ fontSize: `${fontSize}px` }}
+        dangerouslySetInnerHTML={{
+          __html: content ? content : "Ops... Algo deu errado.",
+        }}
+      ></span>
       <section className="chapterContentNavigationFinish">
         <div className="chapterContentFinish">
           <strong>Obrigado por ler até aqui</strong>
-          </div>
+        </div>
         <div className="chapterContentControllerChapter">
-          {theFirst ? 
+          {theFirst ? (
             <strong onClick={goMenu}>Retornar ao menu</strong>
-          : 
+          ) : (
             <strong onClick={goPrev}>{prevChapter}</strong>
-          }
-          {theLast ? 
+          )}
+          {theLast ? (
             <strong onClick={goMenu}>Retornar ao menu</strong>
-          :
+          ) : (
             <strong onClick={goNext}>{nextChapter}</strong>
-          } 
+          )}
         </div>
       </section>
-      
-      <div id="pageProgress" style={{width:`${progressBar}%`}}></div>
+
+      <div id="pageProgress" style={{ width: `${progressBar}%` }}></div>
     </div>
-  )
+  );
 }
