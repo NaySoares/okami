651e3c9184f97eb0c6e5008d33c3350473a21771
Elienai <elienay.soares07@gmail.com>
2021-12-04 20:07:13 -0300

    add categ staff

--- src/services/RandomAvatar.tsx
+++ src/services/RandomAvatar.tsx
@@ -8,11 +8,25 @@ import avatar07 from '../assets/avatar/avatar07.jpg';
 import avatar08 from '../assets/avatar/avatar08.jpg';
 import avatar09 from '../assets/avatar/avatar09.jpg';
 
+import coverCard00 from '../assets/images/cardElaina.jpg';
+import coverCard01 from '../assets/images/coverCard01.png';
+import coverCard02 from '../assets/images/coverCard02.png';
+import coverCard03 from '../assets/images/coverCard03.png';
+import coverCard04 from '../assets/images/coverCard04.jpg';
+import coverCard05 from '../assets/images/coverCard05.jpg';
+import coverCard06 from '../assets/images/coverCard06.jpg';
+
 export function RandomAvatar() {
   const arrAvatar = [avatar01, avatar02, avatar03, avatar04, avatar05, avatar06, avatar07, avatar08, avatar09];
   const Avatar = Math.floor(Math.random() * arrAvatar.length)
-  
   return (
     <img src={arrAvatar[Avatar]} alt="Avatar" /> 
   )
+}
+
+export function RandomCoverCard(){
+  const arrCard = [coverCard00, coverCard01, coverCard02, coverCard03, coverCard04, coverCard05, coverCard06];
+  const Card = Math.floor(Math.random() * arrCard.length)
+  
+  return arrCard[Card];
 }
--- src/services/StaffData.ts
+++ src/services/StaffData.ts
@@ -4,22 +4,46 @@ interface StaffData {
   nick: string;
   category: string[];
   up: uploadsProps;
+  draw?: number;
+  trad?: number;
+  proof?: number;
+  dev: DevProps;
+  adm: boolean;
+  msg: string;
+  coverCard: string;
+}
+interface DevProps {
+  status: boolean;
+  coffee?: number;
+  code?: number;
+}
+interface uploadsProps {
+  majoNoTabitabi: number;
 }
 
-interface uploadsProps{
-    majoNoTabitabi: number,
-}
-
+const randomNumber = Math.floor(Math.random() * 1000);
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export const StaffData: StaffData[] = [
   {
     id: 25,
     avatar: "cover",
     nick: "Axios",
-    category: ["Editor"],
+    category: ["Dev"],
     up: {
       majoNoTabitabi: 8,
-  }},
+    },
+    adm: true,
+    draw: 86,
+    trad: 1000,
+    proof: 2600,
+    dev: {
+      status: true,
+      coffee: 7500,
+      code: 56,
+    },
+    msg: "I tell computer what to do, and something they even do it.",
+    coverCard: "teste",
+  },
   {
     id: 26,
     avatar: "cover",
@@ -27,7 +51,16 @@ export const StaffData: StaffData[] = [
     category: ["Editor"],
     up: {
       majoNoTabitabi: 16,
-  }},
+    },
+    adm: false,
+    draw: 230,
+    proof: 600,
+    dev: {
+      status: false,
+    },
+    msg: "Venci na Vida!",
+    coverCard: "teste",
+  },
   {
     id: 27,
     avatar: "cover",
@@ -35,15 +68,33 @@ export const StaffData: StaffData[] = [
     category: ["Tradutor"],
     up: {
       majoNoTabitabi: 18,
-  }},
+    },
+    adm: false,
+    trad: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "O que você faz em vida ecoa pela eternidade.",
+    coverCard: "teste",
+  },
   {
     id: 28,
     avatar: "cover",
     nick: "Taiyo",
-    category: ["Tradutor"],
+    category: ["Tradutor", "Revisor", "Adm"],
     up: {
       majoNoTabitabi: 6,
-  }},
+    },
+    adm: true,
+    draw: 50,
+    trad: randomNumber,
+    proof: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "Fake love is worse than real hate",
+    coverCard: "teste",
+  },
   {
     id: 29,
     avatar: "cover",
@@ -51,7 +102,15 @@ export const StaffData: StaffData[] = [
     category: ["Revisor"],
     up: {
       majoNoTabitabi: 31,
-  }},
+    },
+    adm: false,
+    proof: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "Rejeite o bom senso para torna o impossível possível",
+    coverCard: "teste",
+  },
   {
     id: 30,
     avatar: "cover",
@@ -59,7 +118,15 @@ export const StaffData: StaffData[] = [
     category: ["Revisor"],
     up: {
       majoNoTabitabi: 3,
-  }},
+    },
+    adm: false,
+    proof: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "Somos todos viajantes em uma estrada de pontos infinitos.",
+    coverCard: "teste",
+  },
   {
     id: 31,
     avatar: "cover",
@@ -67,7 +134,15 @@ export const StaffData: StaffData[] = [
     category: ["Tradutor"],
     up: {
       majoNoTabitabi: 8,
-  }},
+    },
+    adm: false,
+    trad: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "Você não conhece as pessoas, você conhece apenas o que elas permitem que você veja",
+    coverCard: "teste",
+  },
   {
     id: 32,
     avatar: "cover",
@@ -75,7 +150,15 @@ export const StaffData: StaffData[] = [
     category: ["Tradutor"],
     up: {
       majoNoTabitabi: 11,
-  }},
+    },
+    adm: false,
+    trad: randomNumber,
+    dev: {
+      status: false,
+    },
+    msg: "That things we call failure it`s not falling down, but staying down",
+    coverCard: "teste",
+  },
   {
     id: 33,
     avatar: "cover",
@@ -83,5 +166,30 @@ export const StaffData: StaffData[] = [
     category: ["Editor"],
     up: {
       majoNoTabitabi: 9,
-  }},
-]
+    },
+    adm: false,
+    draw: 34,
+    dev: {
+      status: false,
+    },
+    msg: "Kayo, por que me xingas?",
+    coverCard: "teste",
+  },
+  {
+    id: 34,
+    avatar: "cover",
+    nick: "Bravo",
+    category: ["Dev"],
+    up: {
+      majoNoTabitabi: 0,
+    },
+    adm: true,
+    dev: {
+      status: true,
+      coffee: 2100,
+      code: 98,
+    },
+    msg: "Pense positivo e deixe o universo fazer o seu trabalho",
+    coverCard: "teste",
+  },
+];
