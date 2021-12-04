fb4d53d2ac45a681d48499ac019509ce565958bd
Elienai <elienay.soares07@gmail.com>
2021-12-04 20:07:52 -0300

    add dynamic profiles staff

--- src/components/Team/CardMember/CardMember.tsx
+++ src/components/Team/CardMember/CardMember.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { RandomAvatar } from "../../../services/RandomAvatar";
 import translater from "../../../assets/icons/iconTranslate.png";
 import drawer from "../../../assets/icons/iconDrawing.png";
@@ -6,82 +5,89 @@ import developer from "../../../assets/icons/iconDeveloper.png";
 import coffee from "../../../assets/icons/iconCoffee.png";
 import proofreader from "../../../assets/icons/iconProofreading.png";
 import cardElaina from "../../../assets/images/cardElaina.jpg";
+
+import { StaffData } from "../../../services/StaffData";
+import { RandomCoverCard } from '../../../services/RandomAvatar';
 import "./CardMember.scss";
 
 export function CardMember() {
-  const [isDev, setIsDev] = useState(false);
+
   return (
     <main className="cardMemberContainer">
-      <div className="card">
-        <div className="cardUp">
-          <img src={cardElaina} alt="background card" className="bgCard" />
-          <RandomAvatar />
-          <h2 className="titleCard">Axios</h2>
-          <div className="mention">
-            <p>"</p>
-            <span>
-              I tell computer what to do, and something they even do it.
-            </span>
-            <p>"</p>
+      {StaffData.map((member) => {
+        return (
+          <div className="card" key={member.id}>
+            <div className="cardUp">
+              <img src={RandomCoverCard()} alt="background card" className="bgCard" />
+              <RandomAvatar />
+              <h2 className="titleCard">{member.nick}</h2>
+              <div className="mention">
+                <p>"</p>
+                <span>
+                {member.msg}
+                </span>
+                <p>"</p>
+              </div>
+            </div>
+            <div className="boxHidden">
+              <h2>Colaborações</h2>
+              <div className="divisor" />
+              {!member.dev.status ? (
+                <section className="categCard">
+                  <div className="miniBox">
+                    <div className="miniBoxImg">
+                      <img src={translater} alt="icone" />
+                    </div>
+                    <div className="infoMinibox">
+                      <strong>{member.trad ? member.trad : 0}K</strong>
+                      <p>Palavras Traduzidas</p>
+                    </div>
+                  </div>
+                  <div className="miniBox">
+                    <div className="miniBoxImg">
+                      <img src={proofreader} alt="icone" />
+                    </div>
+                    <div className="infoMinibox">
+                      <strong>{member.proof ? member.proof : 0}K</strong>
+                      <p>Palavras Revisadas</p>
+                    </div>
+                  </div>
+                  <div className="miniBox">
+                    <div className="miniBoxImg">
+                      <img src={drawer} alt="icone" />
+                    </div>
+                    <div className="infoMinibox">
+                      <strong>{member.draw ? member.draw : 0}</strong>
+                      <p>Imagens Editadas</p>
+                    </div>
+                  </div>
+                </section>
+              ) : (
+                <section className="categCard">
+                  <div className="miniBox">
+                    <div className="miniBoxImg">
+                      <img src={developer} alt="icone" />
+                    </div>
+                    <div className="infoMinibox">
+                      <strong>{member.dev.code} Hrs</strong>
+                      <p>Horas de código</p>
+                    </div>
+                  </div>
+                  <div className="miniBox">
+                    <div className="miniBoxImg">
+                      <img src={coffee} alt="icone" />
+                    </div>
+                    <div className="infoMinibox">
+                      <strong>{member.dev.coffee}L</strong>
+                      <p>Litros de Café</p>
+                    </div>
+                  </div>
+                </section>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="boxHidden">
-          <h2>Colaborações</h2>
-          <div className="divisor" />
-          {isDev ? (
-            <section className="categCard">
-              <div className="miniBox">
-                <div className="miniBoxImg">
-                  <img src={translater} alt="icone" />
-                </div>
-                <div className="infoMinibox">
-                  <strong>11.5k</strong>
-                  <p>Palavras Traduzidas</p>
-                </div>
-              </div>
-              <div className="miniBox">
-                <div className="miniBoxImg">
-                  <img src={proofreader} alt="icone" />
-                </div>
-                <div className="infoMinibox">
-                  <strong>11.5k</strong>
-                  <p>Palavras Revisadas</p>
-                </div>
-              </div>
-              <div className="miniBox">
-                <div className="miniBoxImg">
-                  <img src={drawer} alt="icone" />
-                </div>
-                <div className="infoMinibox">
-                  <strong>11.5k</strong>
-                  <p>Imagens Editadas</p>
-                </div>
-              </div>
-            </section>
-          ) : (
-            <section className="categCard">
-              <div className="miniBox">
-                <div className="miniBoxImg">
-                  <img src={developer} alt="icone" />
-                </div>
-                <div className="infoMinibox">
-                  <strong>53Hrs</strong>
-                  <p>Horas de código</p>
-                </div>
-              </div>
-              <div className="miniBox">
-                <div className="miniBoxImg">
-                  <img src={coffee} alt="icone" />
-                </div>
-                <div className="infoMinibox">
-                  <strong>0.6L</strong>
-                  <p>Litros de Café</p>
-                </div>
-              </div>
-            </section>
-          )}
-        </div>
-      </div>
+        );
+      })}
     </main>
   );
 }
